6ed41ec9fc3f2bdb2df5fd362d72d49c9a4a9227
CatShredia <sasha990g@gmail.com>
2024-12-15 18:13:09 +0300

    mobile-fons0.21

--- prof/MobileFons/scripts/script.js
+++ prof/MobileFons/scripts/script.js
@@ -2,8 +2,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 
-  let filtersClass = new FiltersClass();
   let finderClass = new FinderClass();
+  let filtersClass = new FiltersClass();
+  let ramClass = new RamClass();
 });
 
 class FiltersClass {
@@ -32,6 +33,8 @@ class FiltersClass {
     });
 
     array.sort();
+
+    console.log(array);
 
     this.min = "От " + array[0];
     this.max = "До " + array[array.length - 1];
@@ -104,6 +107,96 @@ class FinderClass {
           title.parentElement.style.display = "grid";
         }
       });
+    });
+  }
+}
+class RamClass {
+  constructor() {
+    this.costs = document.querySelectorAll(".ram"); //all costs of phones
+    this.forms = document.forms; //all forms
+
+    console.log("filters:");
+    console.log("----");
+    console.log(this.costs);
+    console.log(this.forms);
+    console.log("----");
+
+    this.findMinMax(); //find default costs from html document
+    this.setMinMax(); //set costs to inputs placeholders
+
+    this.applyIvent(); //add apply ivent to button
+  }
+  findMinMax() {
+    let array = [];
+
+    let index = 0;
+    Array.from(this.costs).forEach((cost) => {
+      array[index] = parseInt(cost.textContent.split(" ")[0]);
+      index += 1;
+    });
+
+    array.sort();
+
+    this.min = 50000000;
+    this.max = 0;
+    array.forEach((elem) => {
+      console.log(elem + " s");
+      if (this.min > elem) {
+        this.min = elem;
+      }
+      if (this.max < elem) {
+        this.max = elem;
+      }
+    });
+
+    console.log(array);
+    console.log(this.min + " " + this.max);
+  }
+  setMinMax() {
+    let minInput = this.forms[1].querySelector("#ram-min");
+    let maxInput = this.forms[1].querySelector("#ram-max");
+
+    minInput.setAttribute("placeholder", this.min);
+    maxInput.setAttribute("placeholder", this.max);
+  }
+  applyIvent() {
+    this.forms[1]
+      .querySelector(".apply-button")
+      .addEventListener("click", (ivent) => {
+        //timer to apply
+        setTimeout(
+          this.updateProduct,
+          500,
+          this.forms[1],
+          this.costs,
+          this.min,
+          this.max
+        );
+      });
+  }
+  updateProduct(form, costs, min, max) {
+    // console.log(form);
+    let userMin = form.querySelector("#ram-min").value;
+    let userMax = form.querySelector("#ram-max").value;
+
+    if (userMin == "") {
+      userMin = min;
+    }
+    if (userMax == "") {
+      userMax = max;
+    }
+
+    Array.from(costs).forEach((elem) => {
+      // console.log(parseInt(elem.textContent.split(" ")[0]));
+      // console.log(parseInt(userMin));
+      if (
+        parseInt(elem.textContent.split(" ")[0]) >= parseInt(userMin) &&
+        parseInt(elem.textContent.split(" ")[0]) <= parseInt(userMax)
+      ) {
+        elem.parentElement.style.display = "grid";
+      } else {
+        elem.parentElement.style.display = "none";
+      }
     });
   }
 }
