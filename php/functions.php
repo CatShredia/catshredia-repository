9be52c6ad059f78efce30020469b0744d9b4168e
CatShredia <sasha990g@gmail.com>
2024-12-16 17:58:23 +0300

    php1.1

--- php/functions.php
+++ php/functions.php
@@ -2,16 +2,16 @@
 <?php
 if ($_POST['input'] == 1) {
     print "Первый";
-} 
+}
 ?>
 <br>
 <?php
-if($_POST['input'] == 2) {
+if ($_POST['input'] == 2) {
     // print <br>
     $price = 3.5343241;
     $tax = 0.075;
 
-    printf("The dish costs $%.2f", $price, ($tax + 1) );
+    printf("The dish costs $%.2f", $price, ($tax + 1));
     // <br>
 
     $zip = "6520";
@@ -19,23 +19,28 @@ if($_POST['input'] == 2) {
     $day = 6;
     $year = 2024;
 
-    printf("ZIP is %05d and the date is %02d.%02d.%d",
-    $zip, $month, $day, $year);
-    
+    printf(
+        "ZIP is %05d and the date is %02d.%02d.%d",
+        $zip,
+        $month,
+        $day,
+        $year
+    );
+
     // <br>
-    
+
     $testPerMin = -2;
     $testPerMax = 2;
 
     printf("Max: %+d. Min: %+d", $testPerMin, $testPerMax);
-    
+
     // strcmp()
-    $str1 = "100"; 
-    $str2 = "2100"; 
+    $str1 = "100";
+    $str2 = "2100";
 
     if (strcmp($str1, $str2)) {
         print "Число больше";
-    } else if(strcmp($str1, $str2)) {
+    } else if (strcmp($str1, $str2)) {
         print "Число меньше";
     }
 
@@ -54,13 +59,13 @@ if($_POST['input'] == 2) {
 <br>
 <?php
 
-if($_POST['input'] == 3) {
+if ($_POST['input'] == 3) {
 
     $array = array(
-        'carrot'=> 'parrot',
-        'red'=> 'bet',
-        'redd'=> 'bet',
-        'reddd'=> 'bet'
+        'carrot' => 'parrot',
+        'red' => 'bet',
+        'redd' => 'bet',
+        'reddd' => 'bet'
     );
     $array['black'] = "jhf";
 
@@ -71,7 +76,10 @@ if($_POST['input'] == 3) {
         print $value;
     }
     $arrayNumber = array(
-        1,3,4,65
+        1,
+        3,
+        4,
+        65
     );
     $arrayNumber[0] = 87361;
     print "----------------------";
@@ -86,14 +94,18 @@ if($_POST['input'] == 3) {
 ?>
 <br>
 <?php
-if($_POST['input'] == 4) {
+if ($_POST['input'] == 4) {
     $arrayCh = array(
-        1, 2, 3, 5, 6
+        1,
+        2,
+        3,
+        5,
+        6
     );
-    print in_array('4', $arrayCh) ?'yes':'false';
+    print in_array('4', $arrayCh) ? 'yes' : 'false';
 
     unset($arrayCh[0]);
-    
+
     print " ";
     foreach ($arrayCh as $value) {
         print $value;
@@ -103,9 +115,17 @@ if($_POST['input'] == 4) {
 
 <br>
 <?php
-if($_POST['input'] == 5) {
+if ($_POST['input'] == 5) {
     $array = array(
-        4,34,42,23,23,435,-13,-2,5
+        4,
+        34,
+        42,
+        23,
+        23,
+        435,
+        -13,
+        -2,
+        5
     );
 
     if (in_array(5, $array)) {
@@ -117,19 +137,19 @@ if($_POST['input'] == 5) {
     print array_search(5, $array); //возврат ключа, если есть элемент
     print in_array(5, $array); //возврат true / false, если есть элемент
 
-    foreach ($array as  $value) {
+    foreach ($array as $value) {
         print $value;
         print " ";
     }
     print "---------------";
     sort($array);
-    foreach ($array as  $value) {
+    foreach ($array as $value) {
         print $value;
         print " ";
     }
     print "---------------";
     rsort($array);
-    foreach ($array as  $value) {
+    foreach ($array as $value) {
         print $value;
         print " ";
     }
@@ -138,17 +158,31 @@ if($_POST['input'] == 5) {
 <br>
 
 <?php
-if($_POST['input'] == 6) {
+if ($_POST['input'] == 6) {
     $array = array(
-        1 => ['Hello','World','!!!'],
-        2 => ['World','World','!!!'],
-        3 => ['Hello','Hill','!!!']
+        1 => ['Hello', 'World', '!!!'],
+        2 => ['World', 'World', '!!!'],
+        3 => ['Hello', 'Hill', '!!!']
     );
 
     foreach ($array as $key => $value) {
         print $value[0];
         print $value[1];
         print $value[2];
+    }
+}
+?>
+
+<?php
+$array = array(
+    1,
+    1,
+    2
+);
+
+if ($_POST['input'] == 7) {
+    foreach ($GLOBALS['array'] as $key => $value) {
+        print $value;
     }
 }
 ?>
