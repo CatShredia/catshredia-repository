d01c8231f1217ba76cc80e5756eec8d0bcf044d1
CatShredia <sasha990g@gmail.com>
2025-02-12 12:17:52 +0300

    jquery init v0.1

---
+++ jquery-education/first/script.js
@@ -0,0 +1,27 @@
+console.log("Hi");
+
+// ? nature JavaScript
+// let elems = document.querySelectorAll("p");
+
+// for (let i = 0; i < elems.length; i++) {
+//   elems[i].innerHTML = "!!!";
+//   elems[i].style.color = "red";
+// }
+
+// ? JQuery
+let elem = $(".www");
+elem.css("color", "red").html("!!!");
+
+let text = $(".text").html();
+
+console.log(text);
+
+$(".www").css({ color: "black", font: "20px Arial" });
+
+elem = $("h3 ~ textarea");
+console.log(elem);
+elem = $("h3 + textarea");
+console.log(elem);
+
+let inputsTexts = $("input[type='text']");
+console.log(inputsTexts);
