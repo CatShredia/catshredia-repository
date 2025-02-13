7d1b85d86d215a9e1aa1eb7aa34a73e54a761733
CatShredia <sasha990g@gmail.com>
2025-02-13 16:45:08 +0300

    jquery v0.3

--- jquery-education/second/script.js
+++ jquery-education/second/script.js
@@ -1,3 +1,4 @@
+//
 let texts = $(".main-text li");
 let count = 1;
 
@@ -5,3 +6,33 @@ texts.each(function () {
   $(this).text(`number-${count}`);
   count++;
 });
+
+console.log(texts);
+
+texts = $(".main-text li")
+  .map(function () {
+    return this.id;
+  })
+  .get()
+  .join();
+
+console.log(texts);
+
+console.log($("p"));
+console.log(
+  $("p")
+    .map(function () {
+      return this;
+    })
+    .get()
+    .join(", ")
+);
+
+console.log(
+  $("p")
+    .map(function () {
+      return this.id;
+    })
+    .get()
+    .join()
+);
