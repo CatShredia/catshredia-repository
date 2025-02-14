d9916c0ead26c1f29f01fb77be832fa87a7e9112
CatShredia <sasha990g@gmail.com>
2025-02-14 09:51:07 +0300

    slider-jquery v.1.0

--- jq-slider/slider-script.js
+++ jq-slider/slider-script.js
@@ -1,54 +1,50 @@
 function CreatingSlider() {
   console.log("Creating a new slider");
-
   // кол-во слайдов на странице - countSlideOnPage
-
   //   установка свойств
   sliders = $(".slider-obj");
   $(".slider__inner").css(
     "grid-template-columns",
     "1fr ".repeat(countSlideOnPage).trim()
   );
-
   displaySlides();
 }
 
 let sliders;
-let displaySlidesArray = [0, 2];
-let countSlideOnPage = 3;
-CreatingSlider();
+let displaySlidesArray = [0, 2]; // Начальные индексы для отображения слайдов
+let countSlideOnPage = 3; // Количество слайдов на странице
+let totalSlides = $(".slider-obj").length; // Общее количество слайдов
 
+CreatingSlider();
 
 // событие на левую кнопку
 $("#arr-left").click(function (e) {
   e.preventDefault();
-  
-  displaySlidesArray[0]--;
-  displaySlidesArray[1]--;
-  
-  displaySlides();
+  if (displaySlidesArray[0] > 0) {
+    displaySlidesArray[0]--;
+    displaySlidesArray[1]--;
+    displaySlides();
+  }
 });
+
 // событие на правую кнопку
 $("#arr-right").click(function (e) {
   e.preventDefault();
-  
-  displaySlidesArray[0]++;
-  displaySlidesArray[1]++;
-  
-  displaySlides();
+  if (displaySlidesArray[1] < totalSlides - 1) {
+    displaySlidesArray[0]++;
+    displaySlidesArray[1]++;
+    displaySlides();
+  }
 });
 
 function displaySlides() {
-  console.log(displaySlidesArray)
-
+  console.log(displaySlidesArray);
   clearSliders();
-  
-  for(let i = displaySlidesArray[0];i < displaySlidesArray[displaySlidesArray.length - 1] + 1;i++) {
+  for (let i = displaySlidesArray[0]; i <= displaySlidesArray[1]; i++) {
     $(sliders[i]).css("display", "flex");
-
   }
 }
 
 function clearSliders() {
-  sliders.css("display", "none")
+  sliders.css("display", "none");
 }
