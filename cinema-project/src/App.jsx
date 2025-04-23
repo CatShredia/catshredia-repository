782e34710f067c3f1389e0f650ce2ffa269028f9
CatShredia <sasha990g@gmail.com>
2025-04-23 12:35:26 +0300

    react cinema patch 1 movies counter

--- cinema-project/src/App.jsx
+++ cinema-project/src/App.jsx
@@ -3,14 +3,21 @@ import { useEffect, useState } from "react";
 // компонент card
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
 
+  // deps [hasLiked] используется для того, чтобы функция запускалась, только при изменении hasLiked
   useEffect(() => {
-    console.log(`this movie ${title} is ${hasLiked}`);
-  });
+    console.log(`this movie ${title} is ${hasLiked} ${count}`);
+  }, [hasLiked]);
 
   return (
-    <div className="card">
+    <div
+      className="card"
+      onClick={() => setCount((prevState) => prevState + 1)}
+    >
       <h2>{title}</h2>
+
+      <p className="count">{count || null}</p>
 
       <button className="confirm-button" onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? "❤️" : "😥"}
